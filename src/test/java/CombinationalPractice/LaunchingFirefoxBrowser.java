cafa0aadceff875dbb0673f4dad53fcec1c8bd46
vikra <vikra@Vicky>
2023-06-06 15:12:43 +0530

    commit

--- src/test/java/CombinationalPractice/LaunchingFirefoxBrowser.java
+++ src/test/java/CombinationalPractice/LaunchingFirefoxBrowser.java
@@ -2,11 +2,15 @@ package CombinationalPractice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LaunchingFirefoxBrowser {
 
 	public static void main(String[] args) {
-		WebDriver driver=new ChromeDriver();
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
 		driver.get("https://www.youtube.com/");
 
 	}
