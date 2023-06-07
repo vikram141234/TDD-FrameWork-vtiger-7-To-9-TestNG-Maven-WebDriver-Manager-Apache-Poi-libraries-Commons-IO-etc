cf05af77bd50d15260dc835d6dde7f5dcadb591c
vikra <vikra@Vicky>
2023-06-07 06:36:43 +0530

    commit

--- src/test/java/CombinationalPractice/LaunchingFirefoxBrowser.java
+++ src/test/java/CombinationalPractice/LaunchingFirefoxBrowser.java
@@ -2,6 +2,7 @@ package CombinationalPractice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
